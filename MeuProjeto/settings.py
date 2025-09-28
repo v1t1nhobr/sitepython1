d062de84cf3b3598f32b218c738b91afa4e131ea
Rodrigoo2006 <rdrgsantosaraujo.2021@gmail.com>
2025-09-27 22:35:52 -0300

    FEITO

--- MeuProjeto/settings.py
+++ MeuProjeto/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2gtme_s@ux^n&bjckn7q$p(hihm9aj%)fr16vk6lignz0$du3u'
+SECRET_KEY = 'HFFDJFSOGDSGNGDSKV832558235TYWEGWEIGNDKGVDSVHGSJGLT42TH24UOTHETGFNDSMVWGWGDDVCB-942846444-4999FKFKFKÇÇÇÇÇÇÇÇÇ'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
